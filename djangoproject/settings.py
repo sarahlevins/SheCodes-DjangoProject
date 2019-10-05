6d79d77b9a954c33d3dad86a5aac3a76437dbe2d
sarahlevins <sarahjeanlevins@gmail.com>
2019-10-05 11:48:51 +0800

    update settings.py

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -107,17 +107,17 @@ DATABASES = {
   }
 }
 # Use environment settings for database
-    if 'RDS_HOSTNAME' in os.environ:
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.postgresql_psycopg2',
-                'NAME': os.environ['RDS_DB_NAME'],
-                'USER': os.environ['RDS_USERNAME'],
-                'PASSWORD': os.environ['RDS_PASSWORD'],
-                'HOST': os.environ['RDS_HOSTNAME'],
-                'PORT': os.environ['RDS_PORT'],
-            }
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
         }
+    }
 
 
 
