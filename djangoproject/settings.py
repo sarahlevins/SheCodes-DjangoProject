753a69dbab6ae89c3d5e55c456d12eb231b81943
sarahlevins <sarahjeanlevins@gmail.com>
2019-11-24 17:18:15 +0800

    updates to database and user models

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -13,10 +13,15 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import socket
 import dj_database_url
+import dotenv
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -103,20 +108,8 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "eventfinder",
-        "USER": "sarahlevins",
-        "PASSWORD": "",
-        "HOST": "localhost",
-        "PORT": "5432",
-
-    }
-}
-
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Use environment settings for database
 if 'RDS_HOSTNAME' in os.environ:
@@ -194,4 +187,7 @@ REST_FRAMEWORK = {
     ]
 }
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# This should already be in your settings.py
+django_heroku.settings(locals())
+# This is new
+del DATABASES['default']['OPTIONS']['sslmode']
