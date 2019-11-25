0be8237bddd69ef86290a2c579f139a1b9589778
sarahlevins <sarahjeanlevins@gmail.com>
2019-11-25 12:20:22 +0800

    finishing touches

--- eventfinder/models.py
+++ eventfinder/models.py
@@ -8,14 +8,18 @@ from django.contrib.auth.models import User
 class Event(models.Model):
     name = models.CharField(max_length=75)
     description = models.CharField(max_length=200)
-    start_time = models.DateTimeField('start time and date')
-    end_time = models.DateTimeField('end time and date')
+    start_time = models.DateTimeField(
+        'start time and date (eg. 2019-09-14 11:00:00)')
+    end_time = models.DateTimeField(
+        'end time and date (eg. 2019-09-14 12:00:00)')
     category = models.ManyToManyField('Category', default='')
-    host = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, default='')
+    host = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.DO_NOTHING, default='')
     venue = models.ForeignKey('Venue', on_delete=models.DO_NOTHING, default='')
-    attendees = models.ManyToManyField(User, related_name='attendees', symmetrical=False, default='')
+    attendees = models.ManyToManyField(
+        User, related_name='attendees', symmetrical=False, default='')
     created_date = models.DateTimeField(auto_now_add=True)
-    photo = models.ImageField(upload_to ='event_photos', blank=True)
+    photo = models.ImageField(upload_to='event_photos', blank=True)
 
     def get_absolute_url(self):
         return reverse("eventfinder:event-detail", kwargs={"pk": self.pk})
@@ -23,16 +27,18 @@ class Event(models.Model):
     def __str__(self):
         return self.name
 
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
+
 class Venue(models.Model):
     name = models.CharField(max_length=50)
     venue_website = models.URLField(max_length=100, default='')
     venue_photo = models.ImageField(upload_to='venue_photos', blank=True)
-       
+
     def __str__(self):
         return self.name
